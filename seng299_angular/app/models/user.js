a5b6f26e25fc29056ac561b75b742568a322a67d
cccarr <ccarreng@gmail.com>
2015-07-04 15:08:36 -0700

    Working Login

--- seng299_angular/app/models/user.js
+++ seng299_angular/app/models/user.js
@@ -24,16 +24,29 @@ UserSchema.methods.generateHash = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
+
 UserSchema.pre('save', function(next) {
 	var user = this;
 
 	// hash the password only if the password has been changed or user is new
-	if (!user.isModified('local.password')) return next();
-		//user.local.password = bcrypt.hashSync(user.local.password, bcrypt.genSaltSync(8), null);
-		next();
-	// generate the hash
+	if (!user.isModified('password')) return next();
 
+	// generate the hash
+	bcrypt.hash(user.password, null, null, function(err, hash) {
+		if (err) return next(err);
+
+		// change the password to the hashed version
+		user.password = hash;
+		next();
+	});
 });
+
+// method to compare a given password with the database hash
+UserSchema.methods.comparePassword = function(password) {
+	var user = this;
+
+	return bcrypt.compareSync(password, user.password);
+};
 
 
 // checking if password is valid
--- seng299_angular/app/routes/api.js
+++ seng299_angular/app/routes/api.js
@@ -13,40 +13,57 @@ module.exports = function(app, express) {
 	
 	// route to authenticate a user (POST http://localhost:8080/api/authenticate)
 
+	
+
 	apiRouter.post('/authenticate', function(req, res) {
-		console.log(req.body.username);
 
-		//find the user
-		//select the password explicitly
-		User.findOne({username: req.body.username}).select('password').exec(function(err, user){
+	  // find the user
+	  User.findOne({
+	    username: req.body.username
+	  }).select('name username password').exec(function(err, user) {
 
-			if (err)
-			{
-				throw err;
-			}
+	    if (err) throw err;
 
-			//no user with that username was found
-			if (!user) {
-				res.json({
-					successs: false,
-					message: 'Authentication failed. User not found'
-				});
-			} else {
-			
-				//if user is found and password is correct create a token
-				var token = jwt.sign(user, superSecret, {
-					expiresInMinutes: 1440 //24 hour expiry
-				});
-				
-				//return info including token as JSON
-				res.json({
-					success: true,
-					message: 'Token Created',
-					token: token
-				});
-			}
-		});
+	    // no user with that username was found
+	    if (!user) {
+	      res.json({ 
+	      	success: false, 
+	      	message: 'Authentication failed. User not found.' 
+	    	});
+	    } else if (user) {
+
+	      // check if password matches
+	      var validPassword = user.comparePassword(req.body.password);
+	      if (!validPassword) {
+	        res.json({ 
+	        	success: false, 
+	        	message: 'Authentication failed. Wrong password.' 
+	      	});
+	      } else {
+
+	        // if user is found and password is right
+	        // create a token
+	        var token = jwt.sign({
+	        	name: user.name,
+	        	username: user.username
+	        }, superSecret, {
+	          expiresInMinutes: 1440 // expires in 24 hours
+	        });
+
+	        // return the information including token as JSON
+	        res.json({
+	          success: true,
+	          message: 'Enjoy your token!',
+	          token: token
+	        });
+	      }   
+
+	    }
+
+	  });
 	});
+
+
 
 	//route middleware to verify token
 	apiRouter.use(function(req, res, next) {
